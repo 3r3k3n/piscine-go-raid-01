a3bb511774d1af2b193561f9aa01517bf9ccb8ee
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-19 12:56:24 +0600

    task 4

---
+++ raid1d.go
@@ -0,0 +1,60 @@
+package raid1d
+
+import "github.com/01-edu/z01"
+
+func Raid1d(x, y int) {
+	if x < 0 || y < 0 {
+	} else {
+		if x == 0 || y == 0 {
+		} else {
+			if x == 1 && y == 1 {
+				z01.PrintRune('A')
+				z01.PrintRune(10)
+			} else {
+				if y == 1 {
+					for i := 1; i <= x; i++ {
+						if i == 1 {
+							z01.PrintRune('A')
+						} else if i == x {
+							z01.PrintRune('C')
+						} else {
+							z01.PrintRune('B')
+						}
+					}
+					z01.PrintRune(10)
+				} else {
+					for i := 1; i <= x; i++ {
+						if i == 1 {
+							z01.PrintRune('A')
+						} else if i == x {
+							z01.PrintRune('C')
+						} else {
+							z01.PrintRune('B')
+						}
+					}
+					z01.PrintRune(10)
+					for j := 1; j <= y-2; j++ {
+						for k := 1; k <= x; k++ {
+							if k == 1 || k == x {
+								z01.PrintRune('B')
+							} else {
+								z01.PrintRune(' ')
+							}
+						}
+						z01.PrintRune(10)
+					}
+					for i := 1; i <= x; i++ {
+						if i == 1 {
+							z01.PrintRune('A')
+						} else if i == x {
+							z01.PrintRune('C')
+						} else {
+							z01.PrintRune('B')
+						}
+					}
+					z01.PrintRune(10)
+				}
+			}
+		}
+	}
+}
