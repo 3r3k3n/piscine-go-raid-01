42c9272ddc699fc395dcaa1c0878e02aad856592
Akezhan1 <akezhka.lol@yandex.ru>
2019-10-19 12:09:19 +0600

    task 1

--- raid1a.go
+++ raid1a.go
@@ -2,11 +2,7 @@ package main
 
 import "github.com/01-edu/z01"
 
-<<<<<<< HEAD
 func Raid1a(x,y int) {
-=======
-func Raid1a(x, y int) {
->>>>>>> 89b96d79575da0b9b611a588b4837559dfdf6f9d
 	if x < 0 || y < 0 {
 	} else {
 		if x == 0 || y == 0 {
@@ -16,11 +12,7 @@ func Raid1a(x, y int) {
 				z01.PrintRune(10)
 			} else {
 				if y == 1 {
-<<<<<<< HEAD
 					for i:=1; i <= x; i++ {
-=======
-					for i := 1; i <= x; i++ {
->>>>>>> 89b96d79575da0b9b611a588b4837559dfdf6f9d
 						if i == 1 || i == x {
 							z01.PrintRune('o')
 						} else {
@@ -29,11 +21,7 @@ func Raid1a(x, y int) {
 					}
 					z01.PrintRune(10)
 				} else {
-<<<<<<< HEAD
 					for i:=1; i <= x; i++ {
-=======
-					for i := 1; i <= x; i++ {
->>>>>>> 89b96d79575da0b9b611a588b4837559dfdf6f9d
 						if i == 1 || i == x {
 							z01.PrintRune('o')
 						} else {
@@ -41,13 +29,8 @@ func Raid1a(x, y int) {
 						}
 					}
 					z01.PrintRune(10)
-<<<<<<< HEAD
 					for j:=1; j <= y-2; j++ {
 						for k:=1; k <= x; k++ {
-=======
-					for j := 1; j <= y-2; j++ {
-						for k := 1; k <= x; k++ {
->>>>>>> 89b96d79575da0b9b611a588b4837559dfdf6f9d
 							if k == 1 || k == x {
 								z01.PrintRune('|')
 							} else {
@@ -56,11 +39,7 @@ func Raid1a(x, y int) {
 						}
 						z01.PrintRune(10)
 					}
-<<<<<<< HEAD
 					for i:=1; i <= x; i++ {
-=======
-					for i := 1; i <= x; i++ {
->>>>>>> 89b96d79575da0b9b611a588b4837559dfdf6f9d
 						if i == 1 || i == x {
 							z01.PrintRune('o')
 						} else {
@@ -68,11 +47,7 @@ func Raid1a(x, y int) {
 						}
 					}
 					z01.PrintRune(10)
-<<<<<<< HEAD
 				}	 
-=======
-				}
->>>>>>> 89b96d79575da0b9b611a588b4837559dfdf6f9d
 			}
 		}
 	}
