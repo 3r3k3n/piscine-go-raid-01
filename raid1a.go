2f3f99b03e2f2073864e5e31ddaaf3c68105a164
Akezhan1 <47324992+Akezhan1@users.noreply.github.com>
2019-10-19 11:33:05 +0600

    Update raid1a.go

--- raid1a.go
+++ raid1a.go
@@ -3,8 +3,11 @@ package raid1a
 import "github.com/01-edu/z01"
 
 func Raid1a(x, y int) {
+	if x < 0 || y < 0 {
+		z01.PrintRune('')
+	}
 	if x == 0 || y == 0 {
-		z01.PrintRune('\n')
+		z01.PrintRune('')
 	} else {
 		if x == 1 && y == 1 {
 			z01.PrintRune('o')
